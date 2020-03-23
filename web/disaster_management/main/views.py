313341d56a5cb95ce5e2e5e5c2dc5750135411b4
Mayank Wadhwani <mayankwadhwani@users-MacBook-Air.local>
2020-03-23 17:43:11 +0530

    created the notification form of headquarters dashboard

--- web/disaster_management/main/views.py
+++ web/disaster_management/main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+import pymongo
+from pymongo import MongoClient
+
+def connect():
+    client = MongoClient('mongodb+srv://coderescue:sih2020@trycluster-rfees.mongodb.net/test?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE' , ssl = True)
+    # client = MongoClient('mongodb+srv://user:user@sih-jhvxc.mongodb.net/test?retryWrites=true&w=majority')
+    return client
 
 def index(request):
     context = {}
@@ -9,8 +16,32 @@ def index(request):
 
 #mayank code starts here... kindly accept my part of code if merge conflict arises
 
+locations =["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh","Goa","Gujarat",
+"Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala",
+"Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha",
+"Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand",
+"West Bengal","Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli","Daman and Diu",
+"Lakshadweep","Delhi","Puducherry"]
+
 def headquarters_dashboard(request):
-    return render( request , 'headquarters/dashboard.html' )
+    client = connect()
+    db = client.main.disaster
+    print("HELLO")
+    info = db.find({})
+    data = list(info)
+
+    disaster_names = []
+    location_names = []
+
+    for data in data:
+        disaster_names.append(data["name"])
+    for data in disaster_names:
+        print(data)
+
+    for location in locations :
+        location_names.append(location)
+
+    return render( request , 'headquarters/dashboard.html' , {"disaster_names" : disaster_names , "location_names": location_names} )
 
 def rescue_team_dashboard(request):
     return render( request , 'rescue_team/dashboard.html' )

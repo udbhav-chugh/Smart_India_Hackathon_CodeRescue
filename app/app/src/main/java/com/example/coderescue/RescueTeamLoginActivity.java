86ee16ff16574c2e643266269785d94fc81fca40
Udbhav <udbha170123055@iitg.ac.in>
2020-03-24 07:33:43 +0530

    Started login for rescue team

---
+++ app/app/src/main/java/com/example/coderescue/RescueTeamLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.coderescue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RescueTeamLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rescue_team_login);
+    }
+}

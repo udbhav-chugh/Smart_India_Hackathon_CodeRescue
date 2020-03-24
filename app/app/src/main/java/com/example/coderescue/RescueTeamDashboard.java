de71e5b55acf0e7664c477900c9af2691f06028d
Udbhav <udbha170123055@iitg.ac.in>
2020-03-25 05:21:46 +0530

    Done with rescue team login backend

---
+++ app/app/src/main/java/com/example/coderescue/RescueTeamDashboard.java
@@ -0,0 +1,17 @@
+package com.example.coderescue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mongodb.stitch.android.core.StitchAppClient;
+
+public class RescueTeamDashboard extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rescue_team_dashboard);
+    }
+}

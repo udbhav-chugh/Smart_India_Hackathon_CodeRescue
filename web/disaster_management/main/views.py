78dc6871278fec319c0684477ec0a146992435a3
aman <ar20603@gmail.com>
2020-03-25 02:29:15 +0530

    live notifications: view and js done

--- web/disaster_management/main/views.py
+++ web/disaster_management/main/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.core import serializers
 from django.urls import reverse
 import pymongo
 from pymongo import MongoClient
 from datetime import datetime
+
+locations = ["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh","Goa","Gujarat",
+"Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala",
+"Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha",
+"Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand",
+"West Bengal","Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli","Daman and Diu",
+"Lakshadweep","Delhi","Puducherry"]
 
 def connect():
     client = MongoClient('mongodb+srv://coderescue:sih2020@trycluster-rfees.mongodb.net/test?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE' , ssl = True)
@@ -20,28 +28,49 @@ def index(request):
 def getUserLocation(request):
     if request.method == 'POST':
         location = request.POST['location']
-        request.session['location'] = location
+        # location = location.tolower()
+        if location in locations:
+            request.session['location'] = location
     return HttpResponseRedirect(reverse('main:index'))
 
 def notifications(request, loc):
     client = connect()
     db = client.main.notification
     print("connected")
-    data = db.find()
+    data = db.find().sort("date", pymongo.DESCENDING)
     notfs = list(data)
+    ########################
+    # some error chances bcoz i m considering last time of
+    # or may be not
+    request.session['lastNotification'] = notfs[0]['date']
+    print(notfs[0]['date'])
     context = {
         'notifications' : notfs
     }
     return render(request , 'main/notification.html' , context)
 
-#mayank code starts here... kindly accept my part of code if merge conflict arises
+def get_new_notifications(request):
+    if request.is_ajax and request.method == "GET":
+        lastNotif = request.session['lastNotification']
+        client = connect()
+        db = client.main.notification
+        print("Queried new notifications")
+        data = db.find().sort("date", pymongo.DESCENDING)
+        notfs = list(data)
+        newnotfs = []
+        for notf in notfs:
+            if notf['date'] != lastNotif:
+                ########### since ObjectId is not json serializable
+                notf['_id'] = 0
+                newnotfs.append(notf)
+            # else:
+            #     break
+        return JsonResponse({"new_notifications": newnotfs}, status=200)
+    else:
+        HttpResponseRedirect(reverse('main:index'))
+        ############ change this
 
-locations = ["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh","Goa","Gujarat",
-"Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala",
-"Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha",
-"Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand",
-"West Bengal","Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli","Daman and Diu",
-"Lakshadweep","Delhi","Puducherry"]
+#mayank code starts here... kindly accept my part of code if merge conflict arises
 
 def headquarters_dashboard(request):
     client = connect()

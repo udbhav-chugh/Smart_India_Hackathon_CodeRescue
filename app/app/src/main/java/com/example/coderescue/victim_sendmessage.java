d84923768448c08aea2049c5620824641c5679e7
ajaiswal1818 <abhi@localhost.localdomain>
2020-03-26 13:14:20 +0530

    progress made on victim side

---
+++ app/app/src/main/java/com/example/coderescue/victim_sendmessage.java
@@ -0,0 +1,16 @@
+package com.example.coderescue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class victim_sendmessage extends AppCompatActivity {
+    private Button snd;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_victim_sendmessage);
+    }
+}

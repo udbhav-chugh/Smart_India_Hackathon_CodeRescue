badf720c47335586324ae9a141ffbebb742d446b
Rashi Singh <rashi170101052@iitg.ac.in>
2020-04-02 01:27:57 +0530

    Added basic tab layout in dashboard

---
+++ app/app/src/main/java/com/example/coderescue/Activities/MainDashboardActivity.java
@@ -0,0 +1,29 @@
+package com.example.coderescue.Activities;
+
+import android.os.Bundle;
+
+import com.example.coderescue.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import com.example.coderescue.Adapters.SectionsPagerAdapter;
+
+public class MainDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dashboard);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+}
--- app/app/src/main/java/com/example/coderescue/Fragments/HomeFragment.java
+++ app/app/src/main/java/com/example/coderescue/Fragments/HomeFragment.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.coderescue.Activities.MainDashboardActivity;
 import com.example.coderescue.Activities.RescueTeamLoginActivity;
 import com.example.coderescue.Activities.VictimHomeActivity;
 import com.example.coderescue.Activities.VictimNotifications;
@@ -25,7 +26,7 @@ import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential;
 public class HomeFragment extends Fragment {
 
     public static StitchAppClient client;
-    Button button_helper, button_victim, button_victim_notif;
+    Button button_helper, button_victim, button_victim_notif, button_dashboard;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,6 +34,7 @@ public class HomeFragment extends Fragment {
         button_victim = root.findViewById(R.id.button_victim);
         button_helper = root.findViewById(R.id.button_helper);
         button_victim_notif = root.findViewById(R.id.button_victim_notif);
+        button_dashboard = root.findViewById(R.id.button_dashboard);
 
         button_helper.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +54,13 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), VictimNotifications.class);
+                startActivity(intent);
+            }
+        });
+        button_dashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainDashboardActivity.class);
                 startActivity(intent);
             }
         });
